cd793e2ba7b984d8f3b8e24561d3a101ee86a807
kuldeep7465 <91813803+kuldeep7465@users.noreply.github.com>
2022-05-27 12:06:24 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using dependecy.infratecture;
+using dependecy.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dependecy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly Istudent db;
+        public HomeController(ILogger<HomeController> logger, Istudent _db)
+        {
+            _logger = logger;
+            db = _db;
+        }
+
+        public IActionResult List()
+        {
+            var a = db.GetAll();
+            return View(a);
+        }
+
+        public IActionResult Details(int Id)
+        {
+            var b = db.GEtById(Id);
+            return View(b);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee Employee)
+        {
+           db.Add(Employee);
+            return View();
+        }
+        public IActionResult Delete(int Id)
+        {
+           db.Delete(Id);
+            return View(Id);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Repositry/studentRepo.cs
@@ -0,0 +1,43 @@
+﻿using dependecy.infratecture;
+using dependecy.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dependecy.Repositry
+{
+    public class studentRepo : Istudent
+    {
+        private readonly EmployeeContext _db;
+
+        public studentRepo(EmployeeContext logger)
+        {
+            _db = logger;
+        }
+        public List<Employee> GetAll()
+        {
+
+            return _db.Employees.ToList();
+        }
+
+        public Employee GEtById(int Id)
+        {
+            return _db.Employees.FirstOrDefault(x => x.Id == Id);
+        }
+        public void Add(Employee Emp)
+        {
+            _db.Employees.Add(Emp);
+            _db.SaveChanges();
+        }
+        public void Delete(int Id)
+        {
+            var d= _db.Employees.FirstOrDefault(x => x.Id == Id);
+            _db.Employees.Remove(d);
+            _db.SaveChanges();
+           
+        }
+
+    }
+}
